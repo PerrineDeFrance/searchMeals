d699d580b66da12e83d64363c393324e6c945754
PerrineDeFrance <perrinehuon@gmail.com>
2023-06-07 15:42:13 +0200

    initial commit

---
+++ src/store/state.js
@@ -0,0 +1,11 @@
+export default {
+  searchedMeals: [],
+};
+
+/* 
+cela aurait pu être cela:
+
+searchedMeals: {
+    loading: false, (si on veut mettre qq chose pour dire que ça charge)
+    data: [],
+  },*/
---
+++ src/views/MealsByName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+  </div>
+
+  <div>
+    <pre>{{ meals }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "@vue/reactivity";
+import { ref } from "vue";
+
+import store from "../store";
+
+const keyword = ref(""); //le mot clé qu'on tape dans la barre
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch("searchMeals", keyword.value);
+}
+</script>
